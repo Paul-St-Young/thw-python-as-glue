94c9dd03fa48b48f760dfecb97b23163888ba841
Paul Young <yyang173@illinois.edu>
2019-04-14 22:06:20 -0500

    simplify pyadd

--- 2_simd/pyadd.py
+++ 2_simd/pyadd.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
-import os
 import numpy as np
-
 from forlib import example as fex
 from cpplib import example as cex
 
 def add(i, j):
   return i+j
 
-def check_correct(methods, func_map, vec1, vec2, pyvec):
-  for method in methods:
-    func = func_map[method]
-    vec = func(vec1, vec2)
-    print('%s correct:' % method, np.allclose(vec, pyvec))
+def check_correct(vec1, vec2):
+  pyvec = add(vec1, vec2)
+  cvec = cex.add(vec1, vec2)
+  fvec = cex.add(vec1, vec2)
+  print('cpp=python', np.allclose(cvec, pyvec))
+  print('fortran=python', np.allclose(fvec, pyvec))
 
 def time_func(nrun):
   def wrap(func):
@@ -38,11 +37,11 @@ if __name__ == '__main__':
     'cpp': cex.add
   }
   # check against python answer
-  pyfunc = func_map['python']
-  pyvec = pyfunc(vec1, vec2)
   methods = ['fortran', 'cpp']
-  check_correct(methods, func_map, vec1, vec2, pyvec)
-  nrun = 10000
+  check_correct(vec1, vec2)
+  # compare C and Fortran time against Python
+  pyfunc = func_map['python']
+  nrun = 10000  # number of times to repeat timing measurement
   pytime = time_func(nrun)(pyfunc)(vec1, vec2)
   for method in methods+['python']:
     func = func_map[method]
