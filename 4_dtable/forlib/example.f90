074eae4404c53b38479e3aecf119bd1978b25e18
Paul Young <yyang173@illinois.edu>
2019-04-14 23:03:12 -0500

    simplify fortran distance_table

--- 4_dtable/forlib/example.f90
+++ 4_dtable/forlib/example.f90
@@ -1,17 +1,13 @@
 subroutine distance_table(pos, lbox, natom, ndim, dtable)
-  implicit none
   integer, intent(in) :: natom, ndim
-  real*8, intent(in) :: pos(natom, ndim)
-  real*8, intent(in) :: lbox
+  real*8, intent(in) :: pos(natom, ndim), lbox
   real*8, intent(out) :: dtable(natom, natom)
-  integer i, j, k
+  integer i, j
   real*8 drij(ndim)
   do i=1,natom
     do j=i+1,natom
       drij = pos(i, :) - pos(j, :)
-      do k=1, ndim
-        drij(k) = drij(k) - nint(drij(k)/lbox)*lbox
-      end do
+      drij = drij - lbox*nint(drij/lbox)
       dtable(i, j) = norm2(drij)
       !write (*, '(4f8.4)') drij, dtable(i, j)
     end do
